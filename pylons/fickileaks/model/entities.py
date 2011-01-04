ca8d5344e79c30bbc5f19ae6584926c9784c1f75
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-01-04 23:57:22 +0100

    * database model changes

--- pylons/fickileaks/model/entities.py
+++ pylons/fickileaks/model/entities.py
@@ -1,47 +1,58 @@
+# -*- coding: utf-8 -*-
+
 from elixir import *
 from datetime import datetime
 
 
+class User(Entity):
+    email = Field(Unicode())  # FIXME: build custom email data type
+    password = Field(Unicode())  # FIXME: use hashes instead of plaintext
+
+    # every user has its own relationship “universe”
+    persons = OneToMany('Person')  # a user has many beliefs about persons
+    relations = OneToMany('Relation')  # a user has many beliefs about relations
+
+    def __repr__(self):
+        return '<User %s>' % (self.email)
+
 class Person(Entity):
+    # persons have many names and many URLs
     names = ManyToMany('Name')
-    urls = OneToMany('Url')
+    urls = OneToMany('Url')  # many URLs can denote one person
+
     relations = ManyToMany('Relation')
-    wishes = OneToMany('Relation')
+
+    created = Field(DateTime, default=datetime.now)
+    creator = ManyToOne('User')
 
     def __repr__(self):
         return '<Person %s, %s>' % (self.names, self.urls)
 
-class Url(Entity):
-    url = Field(Unicode(255))
-    person = ManyToOne('Person')
-
-    def __repr__(self):
-        return '<Url %s>' % (self.url)
-
 class Name(Entity):
-    name = Field(Unicode(255))
+    name = Field(Unicode())
     person = ManyToMany('Person')
+
+    created = Field(DateTime, default=datetime.now)
 
     def __repr__(self):
         return '<Name %s>' % (self.name)
 
+class Url(Entity):
+    url = Field(Unicode())  # FIXME: build custom URL data type
+    person = ManyToOne('Person')  # a person can have many URLs
+
+    created = Field(DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return '<Url %s>' % (self.url)
 
 class Relation(Entity):
-    created = Field(DateTime, default=datetime.now)
     participants = ManyToMany('Person')
-    user = ManyToOne('User')
-    type = Field(Unicode(255))
-    wishers = ManyToOne('Person')
+    type = Field(Unicode())  # FIXME: build custom relation data type
+
+    created = Field(DateTime, default=datetime.now)
+    creator = ManyToOne('User')
 
     def __repr__(self):
-        return '<Relation %s %s>' % (self.type, self.participants)
+        return '<Relation %s: %s>' % (self.type, [n.names for n in self.participants])
 
-
-class User(Entity):
-    email = Field(Unicode(255))
-    name = Field(Unicode(255))
-    password = Field(Unicode(255))
-    relations = OneToMany('Relation')
-
-    def __repr__(self):
-        return '<User %s %s>' % (self.name, self.email)
