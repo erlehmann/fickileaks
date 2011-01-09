504a6c057f5aef17e4378c276462800c3f2dc924
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-01-09 03:32:32 +0100

    * base controller class should work with elixir now

--- pylons/fickileaks/lib/base.py
+++ pylons/fickileaks/lib/base.py
@@ -5,7 +5,7 @@ Provides the BaseController class for subclassing.
 from pylons.controllers import WSGIController
 from pylons.templating import render_mako as render
 
-from fickileaks.model.meta import Session
+from fickileaks.model import Session
 
 class BaseController(WSGIController):
 
