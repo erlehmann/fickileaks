5ebdf0535e362d9aafaadf18cd2054a58eca6cbe
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-01-16 14:29:26 +0100

    * use dummy controller

--- pylons/fickileaks/public/js/relationview.js
+++ pylons/fickileaks/public/js/relationview.js
@@ -163,7 +163,7 @@ function radiusFix(g) {
 }
 
 var req = new XMLHttpRequest();
-req.open('GET', 'infovis', true);
+req.open('GET', 'infovis_dummy', true);
 req.onreadystatechange = function (aEvt) {
     if (req.readyState == 4) {
         if(req.status == 200) {
