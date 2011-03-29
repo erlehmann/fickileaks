3333d5aea83bb12a5a27c5477b01b9ec4695467d
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-03-29 22:39:33 +0200

    * fix-folded relations

--- pylons/relationquery-playground.py
+++ pylons/relationquery-playground.py
@@ -19,8 +19,14 @@ def equivalent(node1, node2):
 def merge(node1, node2):
     for name2 in node2.names:
         node1.addName(name2.name)
+
     for url2 in node2.urls:
         node1.addUrl(url2.url)
+
+    for relation2 in node2.relations:
+        pos2 = relation2.participants.index(node2)
+        relation2.participants[pos2] = node1
+    node1.relations.extend(node2.relations)
 
 # get nodes
 for relation in query:
@@ -45,5 +51,14 @@ print "=== FOLDED ==="
 
 for node in nodeset:
     print node
+
+# fix-fold relationships
+for node1 in nodeset:
+    for node2 in nodeset:
+        for relation2 in node2.relations:
+            for participant in relation2.participants:
+                if equivalent(participant, node1):
+                    pos = relation2.participants.index(participant)
+                    relation2.participants[pos] = node1
 
 Session.close()
