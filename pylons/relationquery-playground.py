de7f66d71bc9825419c596213679520fa7bde0c1
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-04-23 01:41:45 +0200

    * looks right somehow

--- pylons/relationquery-playground.py
+++ pylons/relationquery-playground.py
@@ -2,6 +2,8 @@ from fickileaks.model import Session, Relation, Person
 from elixir import metadata
 from json import dumps
 
+from hashlib import md5
+
 metadata.bind = "sqlite:///development.db"
 
 query = Relation.query.all()
@@ -52,17 +54,17 @@ for relation in query:
 for node in nodeset:
     print node
 
-# fold nodes
-while True:
-    try:
-        for node1 in nodeset:
-            for node2 in nodeset:
-                if equivalent(node1, node2):
-                    merge(node1, node2)
-                    nodeset.remove(node2)
-        break
-    except RuntimeError:  #Set changed size during iteration
-        pass
+foldednodeset = set([])
+for node in nodeset:
+    addToFoldedSet = True
+    for foldednode in foldednodeset:
+        if equivalent(node, foldednode):
+            merge(foldednode, node)
+            addToFoldedSet = False
+    if addToFoldedSet:
+        foldednodeset.add(node)
+
+nodeset = foldednodeset
 
 print "=== FOLDED ==="
 
@@ -76,83 +78,65 @@ print "=== RELATIONS FIXED ==="
 for node in nodeset:
     print node
 
-exit
+print md5(str(nodeset)).hexdigest()
 
-## fix-fold relations
-#for node1 in nodeset:
-    #for node2 in nodeset:
-        #for relation1 in node1.relations:
-            #for participant in relation1.participants:
-                #if equivalent(participant, node2):
-                    #pos = relation1.participants.index(participant)
-                    #relation1.participants[pos] = node2
-        #try:
-            #for relation2 in node2.relations:
-                #for participant in relation2.participants:
-                    #if equivalent(participant, node1):
-                        #pos = relation2.participants.index(participant)
-                        #relation2.participants[pos] = node1
-        #except IntegrityError:
-            #pass  # FIXME: I have no idea why this is happening sometimes
-            
+print "=== JSONIFIED ==="
 
-#print "=== JSONIFIED ==="
+from pprint import pprint
+pprint(nodeset)
 
-#from pprint import pprint
-#pprint(nodeset)
+# serialize structure so it can be jsonified
+nodelist = []
 
-## serialize structure so it can be jsonified
-#nodelist = []
+for node in nodeset:
+    serialnode = {
+        'id': node.urls[0].url,
+        'name': node.names[0].name,
+        'data': {
+            # set hack used so both Names and URLs occur only once
+            'names': list(set([n.name for n in node.names])),
+            'urls': list(set([u.url for u in node.urls]))
+        },
+        'adjacencies': []
+    }
 
-#for node in nodeset:
-    #serialnode = {
-        #'id': node.urls[0].url,
-        #'name': node.names[0].name,
-        #'data': {
-            ## set hack used so both Names and URLs occur only once
-            #'names': list(set([n.name for n in node.names])),
-            #'urls': list(set([u.url for u in node.urls]))
-        #},
-        #'adjacencies': []
-    #}
+    for relation in node.relations:
+        for participant in relation.participants:
+            if not equivalent(node, participant):
 
-    #for relation in node.relations:
-        #for participant in relation.participants:
-            #if not equivalent(node, participant):
+                adjacencynodeexists = False
+                relationnodeexists = False
+                for adjacency in serialnode['adjacencies']:
+                    if (adjacency['nodeTo'] == participant.urls[0].url):
+                        adjacencynodeexists = True
+                        adjacencynode = adjacency
+                    for relation0 in adjacency['data']['relations']:
+                        if (relation0['type'] == relation.type):
+                            relationnodeexists = True
+                            relationnode = relation0
 
-                #adjacencynodeexists = False
-                #relationnodeexists = False
-                #for adjacency in serialnode['adjacencies']:
-                    #if (adjacency['nodeTo'] == participant.urls[0].url):
-                        #adjacencynodeexists = True
-                        #adjacencynode = adjacency
-                    #for relation0 in adjacency['data']['relations']:
-                        #if (relation0['type'] == relation.type):
-                            #relationnodeexists = True
-                            #relationnode = relation0
+                if not adjacencynodeexists:
+                    adjacencynode = {
+                        'nodeTo': participant.urls[0].url,
+                        'data': {
+                            'relations': []
+                        }
+                    }
+                    serialnode['adjacencies'].append(adjacencynode)
 
-                #if not adjacencynodeexists:
-                    #adjacencynode = {
-                        #'nodeTo': participant.urls[0].url,
-                        #'data': {
-                            #'relations': []
-                        #}
-                    #}
-                    #serialnode['adjacencies'].append(adjacencynode)
-
-                #if not relationnodeexists:
-                    #relationnode = {
-                        #'type': relation.type,
-                        #'creators': [relation.creator.email],
-                    #}
+                if not relationnodeexists:
+                    relationnode = {
+                        'type': relation.type,
+                        'creators': [relation.creator.email],
+                    }
                 
-                    #adjacencynode['data']['relations'].append(relationnode)
-                #else:
-                    #if not (relation.creator.email in relationnode['creators']):
-                        #relationnode['creators'].append(relation.creator.email)
+                    adjacencynode['data']['relations'].append(relationnode)
+                else:
+                    if not (relation.creator.email in relationnode['creators']):
+                        relationnode['creators'].append(relation.creator.email)
                     
 
-    #nodelist.append(serialnode)
+    nodelist.append(serialnode)
 
 #print dumps(nodelist, sort_keys=True, indent=2)
 
